3c69b3d5614c975a5b287a5be37d267427575c40
Leslie Xin <lesliexin@v1020-wn-75-231.campus-dynamic.uwaterloo.ca>
2018-09-15 05:08:50 -0400

    Added flask server and sms response

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
