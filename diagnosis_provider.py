7d428c20150c3c3fdf9195d11e43569fbaea0254
Sherry Li <sherryhli@users.noreply.github.com>
2018-09-15 12:22:36 -0400

    Automated access_token retrieval

--- diagnosis_provider.py
+++ diagnosis_provider.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from access_token_provider import get_access_token
 
 # Custom exception class for errors thrown by API call
 class ApiError(Exception):
@@ -10,8 +11,7 @@ class ApiError(Exception):
 
 # Gets the JSON object containing diagnosis data
 def get_diagnosis_json(symptoms, gender, year_of_birth):
-    # Will write separate funtion to generate this (changes every 2 hours)
-    api_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNoZXJyeWhsaUBob3RtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTExNSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjEwOCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiIxMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJCYXNpYyIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTgtMDktMTUiLCJpc3MiOiJodHRwczovL2F1dGhzZXJ2aWNlLnByaWFpZC5jaCIsImF1ZCI6Imh0dHBzOi8vaGVhbHRoc2VydmljZS5wcmlhaWQuY2giLCJleHAiOjE1MzcwMzMxOTQsIm5iZiI6MTUzNzAyNTk5NH0.zqPwR_UXDrpLASO0dyzEpqQVvYlfW-VTKvYsYsAB9Fo"
+    api_token = get_access_token()
     api_url = "https://healthservice.priaid.ch/diagnosis"
     symptoms_string = ""
     for i in range(len(symptoms)):
