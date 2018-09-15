ec356ef050bb35b407e0b431a95c096b0121a6bd
Leslie Xin <lesliexin@v1020-wn-75-231.campus-dynamic.uwaterloo.ca>
2018-09-15 14:06:40 -0400

    [LX] Greeting user based on if they have texted before, asking for name

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ callers = {
 @app.route("/sms", methods=['GET', 'POST'])
 def hello():
     """Respond with the number of text messages sent between two parties."""
+
     # Increment the counter
     counter = session.get('counter', 0)
     counter += 1
@@ -24,15 +25,46 @@ def hello():
     # Save the new counter value in the session
     session['counter'] = counter
 
+    # get user text
+    text = request.values.get('Body', None)
+
+
     from_number = request.values.get('From')
     if from_number in callers:
         name = callers[from_number]
+        counter += 1
     else:
-        name = "Friend"
+        name = ""
+
+    message = ''
+
+    # new user
+    if counter == 1:
+        # ask for name 
+        message = 'Hi there, what is your name? counter is {}' \
+            .format( counter)
+
+    elif counter == 2:
+        # greet user]
+
+        if name == "":
+            callers[str(from_number)] = str(text)
+            message = 'Hi there, {}! Welcome to textMD! counter is {}' \
+                .format(text, counter)
+
+        else:
+            message = 'Welcome back, {}! counter is {}' \
+                .format(name, counter)
+
+    else:
+        message = 'counter is {}' \
+            .format(name, counter)
+        session.clear()
+
 
     # Build our reply
-    message = '{} has messaged {} {} times.' \
-        .format(name, request.values.get('To'), counter)
+    # message = '{} has messaged {} {} times.' \
+    #     .format(name, request.values.get('To'), counter)
 
     # Put it in a TwiML response
     resp = MessagingResponse()
